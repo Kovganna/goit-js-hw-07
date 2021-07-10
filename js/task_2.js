407288a8d484028febde790201aa5530ceb8018d
Ganna <annakovalenko168@gmail.com>
2021-07-10 16:08:47 +0300

    task 1

---
+++ js/task_2.js
@@ -0,0 +1,87 @@
+// Задание 2
+// В HTML есть пустой список ul#ingredients.
+
+// <ul id="ingredients"></ul>
+// В JS есть массив строк.
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li, после чего вставит все li за одну операцию в список ul.ingredients. Для создания DOM-узлов используй document.createElement().
+
+const itemFirstEl = document.createElement('li');
+itemEl.classList.add('list-item');
+itemEl.textContent ='Картошка';
+console.log(itemEl);
+
+document.body.appendChild(itemEl);
+const itemEl = document.createElement('li');
+itemEl.classList.add('list-item');
+itemEl.textContent = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+console.log(itemEl);
+
+document.body.appendChild(itemEl);
+const itemEl = document.createElement('li');
+itemEl.classList.add('list-item');
+itemEl.textContent = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+console.log(itemEl);
+
+document.body.appendChild(itemEl);
+const itemEl = document.createElement('li');
+itemEl.classList.add('list-item');
+itemEl.textContent = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+console.log(itemEl);
+
+document.body.appendChild(itemEl);
+const itemEl = document.createElement('li');
+itemEl.classList.add('list-item');
+itemEl.textContent = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+console.log(itemEl);
+
+document.body.appendChild(itemEl);
+const itemEl = document.createElement('li');
+itemEl.classList.add('list-item');
+itemEl.textContent = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+console.log(itemEl);
+
+document.body.appendChild(itemEl);
