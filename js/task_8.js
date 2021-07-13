7d50fe08c1e14802972a3da5dd86535730b417c7
Ganna <annakovalenko168@gmail.com>
2021-07-13 13:16:14 +0300

    task_8

--- js/task_8.js
+++ js/task_8.js
@@ -17,3 +17,35 @@
 // </div>
 
 // <div id="boxes"></div>
+
+ 
+let render = document.querySelector('[data-action="render"]');
+let destroy = document.querySelector('[data-action="destroy"]');
+let boxes = document.getElementById('boxes');
+render.addEventListener('click', getAmount);
+destroy.addEventListener('click', destroyBoxes);
+
+function getAmount() {
+  let amount = +document.querySelector('#controls input').value;
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  let size = 30;
+  let fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i++) {
+    let newSize = size + i * 10;
+    let div = document.createElement('div');
+    div.style.cssText = `width: ${newSize}px; height: ${newSize}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+function random() {
+  return Math.floor(Math.random() * 256);
+}
